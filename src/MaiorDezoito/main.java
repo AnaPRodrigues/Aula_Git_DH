e2009224659c7c8e126943fe8251a056424aa345
AnaPRodrigues <aninha-felicio@hotmail.com>
2022-05-20 18:57:58 -0300

    Commit Teste

---
+++ src/MaiorDezoito/main.java
@@ -0,0 +1,25 @@
+package MaiorDezoito;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		Scanner Ler = new Scanner(System.in);
+		int idade = 0;
+		int qtde = 0;
+		int i = 1;
+		System.out.println("Programa que determina quantas pessoas possuem 18 anos ou mais");
+		while(i <= 10){
+		System.out.print("Informe a idade da "+i+" pessoa: ");
+		idade = Integer.parseInt(Ler.nextLine());
+		if(idade >= 18){
+		qtde++;
+		}
+		i = i +1;
+		}
+		System.out.println("Existem "+qtde+" pessoas que possuem 18 anos ou mais");
+
+	}
+
+}
